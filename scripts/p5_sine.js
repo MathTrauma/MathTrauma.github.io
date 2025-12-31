ba1654afacdb29c5a28d62ce5193e87200fd5627
mathtrauma <mathtrauma@yahoo.com>
2025-12-31 14:36:10 +0900

    trouble

--- scripts/p5_sine.js
+++ scripts/p5_sine.js
@@ -1,4 +1,4 @@
-import p5 from 'https://cdnjs.cloudflare.com/ajax/libs/p5.js/1.7.0/p5.min.js';
+// import p5 from 'https://cdnjs.cloudflare.com/ajax/libs/p5.js/1.7.0/p5.min.js';
 
 
 export function createP5Sine(sketchParent) {
